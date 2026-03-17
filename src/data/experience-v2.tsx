9cf438668c8816db020472312125e501a768c174
locnv <locnv14@gmail.com>
2026-03-17 11:31:33 +0700

    Update experience section layout and improve image shadow styling
    - Changed the grid layout in the experience section from 3 columns to 2 for better responsiveness.
    - Refined shadow styling for images to enhance visual consistency and quality.

--- src/data/experience-v2.tsx
+++ src/data/experience-v2.tsx
@@ -176,20 +176,20 @@ export const getExperienceTimelineV2 = (t: (key: string) => string) => [
           </div>
         </div>
 
-        <div className="grid md:grid-cols-3 grid-cols-1 gap-4">
+        <div className="grid md:grid-cols-2 grid-cols-1 gap-4">
           <BlurImage
             src="/images/brando/brando-2.png"
             alt="startup template"
             width={500}
             height={500}
-            className="rounded-lg object-cover h-full w-fit shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+            className="rounded-lg object-cover h-full w-fit shadow-[0_0_24px_rgba(34,42,53,0.06),0_1px_1px_rgba(0,0,0,0.05),0_0_0_1px_rgba(34,42,53,0.04),0_0_4px_rgba(34,42,53,0.08),0_16px_68px_rgba(47,48,55,0.05),0_1px_0_rgba(255,255,255,0.1)_inset]"
           />
           <BlurImage
             src="/images/brando/brando-1.jpg"
             alt="startup template"
             width={500}
             height={500}
-            className="rounded-lg object-cover h-full w-fit shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+            className="rounded-lg object-cover h-full w-fit shadow-[0_0_24px_rgba(34,42,53,0.06),0_1px_1px_rgba(0,0,0,0.05),0_0_0_1px_rgba(34,42,53,0.04),0_0_4px_rgba(34,42,53,0.08),0_16px_68px_rgba(47,48,55,0.05),0_1px_0_rgba(255,255,255,0.1)_inset]"
           />
         </div>
       </div>
